5a4dd01dd2f794d5cac85cd397d5526f556b0b6d
Olumide Olubosede <olumideolubosede@gmail.com>
2024-01-04 09:26:55 +0100

    ...

--- lib/general_components/navigation_bar_item.dart
+++ lib/general_components/navigation_bar_item.dart
@@ -4,44 +4,44 @@ class NavigationBarItem extends StatelessWidget {
   final IconData icon;
   final String title;
   final bool isActive;
-  final onPressed;
+  final VoidCallback onPressed;
+  final Color activeIconColor;
+  final Color inactiveIconColor;
 
-  NavigationBarItem({
+  const NavigationBarItem({
+    super.key,
     required this.icon,
     required this.title,
     required this.onPressed,
     required this.isActive,
+    this.activeIconColor = Colors.black,
+    this.inactiveIconColor = Colors.grey,
   });
-
-  final activeIconColor = Colors.black;
-  final inactiveIconColor = Colors.grey[500];
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 2),
+        margin: const EdgeInsets.symmetric(horizontal: 2),
         child: MaterialButton(
           onPressed: onPressed,
           elevation: 0,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-          padding: EdgeInsets.all(0),
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+          padding: const EdgeInsets.all(0),
           color: Colors.white,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Icon(
                 icon,
-                color: isActive == true
-                    ? activeIconColor : inactiveIconColor,
+                color: isActive == true ? activeIconColor : inactiveIconColor,
               ),
               Text(
                 title,
                 overflow: TextOverflow.ellipsis,
                 style: TextStyle(
                   fontSize: 10,
-                  color: isActive == true
-                    ? activeIconColor : inactiveIconColor,
+                  color: isActive == true ? activeIconColor : inactiveIconColor,
                 ),
               ),
             ],
